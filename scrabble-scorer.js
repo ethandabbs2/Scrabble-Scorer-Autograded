7d094e830fb3e6573146ade154bb33a95f45c5d7
ethandabbs2 <ethandabbs2@yahoo.com>
2023-11-02 13:28:44 -0500

    graded assignment 2

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,27 +33,87 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let initialWord = (input.question("Let's play some scrabble! \n\nEnter a word:"));
+   console.log(oldScrabbleScorer(initialWord));
 };
 
-let simpleScorer;
 
-let vowelBonusScorer;
+let simpleScorer = function(word) {
+   return word.length;
+}
 
-let scrabbleScorer;
+let vowels = "AEIOU";
+let vowelBonusScorer = function(word) {
+   word = word.toUpperCase();
+   let score = 0;
+   for (char in word) {
+      if (vowels.includes(word[char])) {
+         score += 3;
+      } else score += 1;
+   }
+      return score;
+  }   
 
-const scoringAlgorithms = [];
+let scrabbleScorer = function(word) {
+   let score = 0;
+   for (let index in word) {
+      letter = word[index];
+      score += Number(newPointStructure[letter]);
+   }
+   return score;
+}
 
-function scorerPrompt() {}
+const scoringAlgorithms = [
+   {
+      name: "Simple Score",
+      description: "Each letter is worth 1 point.",
+      scorerFunction: simpleScorer
+   },
+   {
+      name: "Bonus Vowels",
+      description: "Vowels are 3 pts, consonants are 1 pt.",
+      scorerFunction: vowelBonusScorer
+   },
+   {
+      name: "Scrabble",
+      description: "The traditional scoring algorithm.",
+      scorerFunction: scrabbleScorer
+   }
+];
 
-function transform() {};
+function scorerPrompt() {
+   let scoreChoice = Number(input.question("\nWhich scoring algorithm would you like to use? \n0- Simple: One point per character \n1- Vowel Bonus: Vowels are worth 3 points \n2- Scrabble: Uses scrabble point system \n\nEnter 0, 1, or 2: "));
 
-let newPointStructure;
+   if (scoreChoice === 1) {
+      let andSometimesY = input.question("Would you like to count Y as a vowel? ");
+
+      if (andSometimesY.toUpperCase() === "Y") {
+         vowels += "Y";
+      }
+   }
+   
+   return scoringAlgorithms[scoreChoice];
+}
+
+function transform(pointStructure) {
+   let newStructure = {};
+   for(score in pointStructure) {
+      for(let i = 0; i < pointStructure[score].length; i++) {
+         newStructure[pointStructure[score][i].toLowerCase()] = Number(score);
+      }
+   }
+
+   return newStructure;
+};
+
+let newPointStructure = transform(oldPointStructure);
 
 function runProgram() {
-   initialPrompt();
-   
-}
+   console.log("Let's play some scrabble!\n");
+      let userWord = input.question( "Enter a word to score: ");
+      let sortingAlgorithm = scorerPrompt();
+      console.log(`Score for '${userWord}': ${sortingAlgorithm.scorerFunction(userWord)}`);
+   }
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
